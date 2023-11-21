fc9654bcfc42e787385d5c99fcc1211bf0672f8d
houjityaumai <houjitya1910@gmail.com>
2023-11-21 11:38:07 +0900

    [add] user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,42 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(get_user)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(get_user)
+      redirect_to users_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
   end
 
   def login
@@ -25,4 +44,10 @@ class UsersController < ApplicationController
 
   def logout
   end
+
+  private
+  def get_user
+    params.require(:user).permit(:name, :email, :password)
+  end
+
 end
