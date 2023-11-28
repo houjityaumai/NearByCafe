4d9f72186bb1d30bb45f013946d0c2030f881d98
houjityaumai <houjitya1910@gmail.com>
2023-11-28 12:26:14 +0900

    [correction] shop.rb claasname

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -9,13 +9,38 @@ class ShopsController < ApplicationController
 
   def position
     yahoo_key = "dj00aiZpPTBlMlZsNXRFck5nOSZzPWNvbnN1bWVyc2VjcmV0Jng9YjI-"
-    url = URI.parse("https://map.yahooapis.jp/search/local/V1/localSearch?appid=#{yahoo_key}&lat=35.665662327484&lon=139.73091159273&dist=3")
+    url = URI.parse("https://map.yahooapis.jp/search/local/V1/localSearch?appid=#{yahoo_key}&lat=35.665662327484&lon=139.73091159273&dist=1")
     req = Net::HTTP::Get.new(url.request_uri)
     ret = Net::HTTP::start(url.host, url.port, use_ssl: true) do |http|
       http.request(req)
     end
     xml = Nokogiri::XML(ret.body)
-    logger.debug(xml)
+
+    # 名前空間の定義
+    namespace = {
+      "YDF" => "http://olp.yahooapis.jp/ydf/1.0"
+    }
+
+
+    feature = xml.xpath("//YDF:Feature", namespace)
+    info_array = []
+    @shops = []
+
+    feature.each do |f|
+      info_array << [f.xpath("YDF:Name", namespace).text, f.xpath("YDF:Property/YDF:Address", namespace).text]
+    end
+
+    info_array.each do |f|
+      @shops << Utils::Shop.new(f[0], f[1])
+      logger.debug(f[0])
+      logger.debug(f[1])
+    end
+    logger.debug(@shops)
+
+
+    # logger.debug("-----------------------------------------")
+    # logger.debug(feature)
+    # logger.debug("-----------------------------------------")
   end
 
   def search
--- lib/class/shop.rb
+++
@@ -1,7 +0,0 @@
-class Shop_Info
-  attr_accessor :name, :address
-  def initalize(name, address)
-    self.name = name
-    self.address = address
-  end
-end
