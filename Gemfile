bc760f151f878e6053740fcb7b7a76737ecd00aa
houjityaumai <houjitya1910@gmail.com>
2023-12-05 12:55:43 +0900

    [add] js_config...etc [chenge] yahooapi => hotpepoerapi

--- Gemfile
+++ Gemfile
@@ -51,3 +51,10 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# Bootstrap用
+gem 'jquery-rails'
+gem 'bootstrap', '4.6.1'
+
+#ホットペッパーapi
+gem 'httparty'
--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,6 +1,8 @@
 class ShopsController < ApplicationController
-  require 'nokogiri'
   require 'net/http'
+  require 'uri'
+  require 'json'
+
   def index
   end
 
@@ -8,33 +10,35 @@ class ShopsController < ApplicationController
   end
 
   def position
-    yahoo_key = "dj00aiZpPTBlMlZsNXRFck5nOSZzPWNvbnN1bWVyc2VjcmV0Jng9YjI-"
-    url = URI.parse("https://map.yahooapis.jp/search/local/V1/localSearch?appid=#{yahoo_key}&lat=35.665662327484&lon=139.73091159273&dist=1")
+  
+
+
+
+
+    yahoo_key = "64021912cf2b3b35"
+    url = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/")
+    url.query = URI.encode_www_form({
+      key: yahoo_key,
+      lat: 35.4622429,
+      lng: 133.0664864,
+      range: 5 ,
+      genre: 'G014', # カフェを選択
+      keyword: params[:keyword],
+      large_area: params[:large_area],
+      count: 20,
+      format: 'json'
+    })
     req = Net::HTTP::Get.new(url.request_uri)
-    ret = Net::HTTP::start(url.host, url.port, use_ssl: true) do |http|
+    ret = Net::HTTP::start(url.host, url.port) do |http|
       http.request(req)
     end
-    xml = Nokogiri::XML(ret.body)
-
-    # 名前空間の定義
-    namespace = {
-      "YDF" => "http://olp.yahooapis.jp/ydf/1.0"
-    }
-
-
-    feature = xml.xpath("//YDF:Feature", namespace)
-    info_array = []
+    json = JSON.parse(ret.body)
+    logger.debug(json["results"]["shop"])
     @shops = []
-
-    feature.each do |f|
-      info_array << [f.xpath("YDF:Name", namespace).text, f.xpath("YDF:Property/YDF:Address", namespace).text]
+    json["results"]["shop"].each do |shop|
+      @shops << Utils::Shop.new(shop["name"], shop["address"])
     end
 
-    info_array.each do |f|
-      @shops << Utils::Shop.new(f[0], f[1])
-      logger.debug(f[0])
-      logger.debug(f[1])
-    end
   end
 
   def search
