99b8be59b204a3e5f52603d379c85ec4ecce3686
unknown <JR2204@bijisen.local>
2024-01-15 14:44:31 +0900

    kyouhaowari

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-<<<<<<< HEAD
-
+  has_many :likes, dependent: :destroy
   validates :name,
     presence: true,
     length: {
@@ -16,7 +15,5 @@ class User < ApplicationRecord
   length: {
     maximum: 300
   }
-=======
-  has_many :likes, dependent: :destroy
->>>>>>> 0ae8c69b0b4463c167568b981519d84011af1f83
+
 end
