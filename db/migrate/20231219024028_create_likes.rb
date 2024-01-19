381274c7733a676f3dbcf9b633ee30cd0df03ca5
sota <black_yku@icloud.com>
2024-01-19 13:01:53 +0900

    aaa

--- db/migrate/20231219024028_create_likes.rb
+++ db/migrate/20231219024028_create_likes.rb
@@ -2,7 +2,7 @@ class CreateLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :likes do |t|
       t.integer :user_id
-      t.integer :shop_id
+      t.string :shop_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2023_12_19_024028) do
 
-  create_table "favorites", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "like_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "shop_id"
+    t.string "shop_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
